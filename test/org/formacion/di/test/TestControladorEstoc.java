1d7ca0597b7210a5c0e87fc791904027070dfe20
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2020-04-22 17:46:52 +0200

    test(TestControladorEstoc): Cambiar lógica del caso test

--- test/org/formacion/di/test/TestControladorEstoc.java
+++ test/org/formacion/di/test/TestControladorEstoc.java
@@ -1,6 +1,8 @@
 package org.formacion.di.test;
 
+import org.formacion.di.bbdd.Inventario;
 import org.formacion.di.bbdd.InventarioBBDD;
+import org.formacion.di.negocio.Controlador;
 import org.formacion.di.negocio.ControladorEstoc;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,7 +11,8 @@ public class TestControladorEstoc {
 
 	@Test
 	public void test_control_estoc() {
-		ControladorEstoc controlador = new ControladorEstoc(new InventarioBBDD());
+		Inventario inventario = new InventarioBBDD();
+		Controlador controlador = new ControladorEstoc(inventario);
 		
 		Assert.assertFalse(controlador.necesitaReponer("tienda norte", "mesa"));
 		Assert.assertTrue(controlador.necesitaReponer("tienda norte", "lampara"));
